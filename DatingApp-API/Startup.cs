9bbe681254545d0d891acbf764bd76a72a38194d
William Newman <billycoda@gmail.com>
2020-05-12 18:40:44 -0600

    First class init

--- DatingApp-API/Startup.cs
+++ DatingApp-API/Startup.cs
@@ -36,7 +36,7 @@ namespace DatingApp_API
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseHttpsRedirection();
+            // app.UseHttpsRedirection();
 
             app.UseRouting();
 
